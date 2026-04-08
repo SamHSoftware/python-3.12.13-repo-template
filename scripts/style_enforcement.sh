d7fb3564bb1b0fc868c0a39e124cc5eb8c801bea
Samuel Huguet <samhuguet@brokenstringbio.com>
2026-04-09 00:10:41 +0200

    Feat: Adding lint.yml.

--- scripts/style_enforcement.sh
+++ scripts/style_enforcement.sh
@@ -2,12 +2,14 @@
 
 # Black linting.
 printf "➡️  Linting with black.\n"
-black . 
+black .                    
+black --check . # Give warnings for remaining errors - as in CI. Catch it sooner!
 printf '\n'
 
 # isort linting.
 printf "➡️  Linting with isort.\n"
 isort .
+isort . --check # Same as with black.
 printf '\n'
 
 # flake8 style enforcement.
